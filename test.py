592a17d2001c01c1cc2f16e5896e781105a7e80a
DevFanHM <no_spam_me_bro@yahoo.com>
2017-06-05 19:58:45 +0200

    Update for test.py, might come to test.cpp

--- test.py
+++ test.py
@@ -2,3 +2,12 @@
 for i in range(101):
     for s in range(i):
         print (s * i)
+def BinaryPyramid(size):
+	l = 1
+	for i in range(size):
+		if i % 2 == 0:
+			print (' ' * (size - i) + '1' * l)
+		else:
+			print (' ' * (size - i) + '0' * l)
+		l += 2
+BinaryPyramid(40)
