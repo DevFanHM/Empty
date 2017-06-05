4603677b3483c391d64c6a49f7a2b847dfcd0acb
DevFanHM <no_spam_me_bro@yahoo.com>
2017-06-05 18:15:22 +0200

    Added test.cpp and fixed test.py.

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+  for (int i = 0; i <= 100; i++)
+  {
+    for (int s = 0; s<= 100; s++)
+    {
+      cout << s * i << endl;
+    }
+  }
+}
