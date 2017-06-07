195c3e783dd6a49799ec8bf7f336aaa73978f21c
DevFanHM <no_spam_me_bro@yahoo.com>
2017-06-07 19:54:47 +0200

    Commented out the old test legacy code in test.py Gave test.py random digits for the pyramid

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 # Welcome, this is a simple, pretty useless, test python file.
-for i in range(101):
-    for s in range(i):
-        print (s * i)
+#The main test legacy code is commented out.
+#for i in range(101):
+#    for s in range(i):
+#        print (s * i)
+import random
+def ranbinstr(Length):
+    def digit():
+        BinDigit = ''
+        while (len(BinDigit) < Length):
+            BinDigit += random.choice('0' '1')
+        return BinDigit
+    return digit()
 def BinaryPyramid(size):
 	l = 1
 	for i in range(size):
-		if i % 2 == 0:
-			print (' ' * (size - i) + '1' * l)
-		else:
-			print (' ' * (size - i) + '0' * l)
-		l += 2
+            print (' ' * (size - i) + ranbinstr(l))
+            l += 2
 BinaryPyramid(40)
